ccfa5c89fd9ce34106066b182e725199ea558515
Torpid <mistermohan02@gmail.com>
2024-08-28 23:04:03 +0530

    New changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://mishravidit02:DlUREwPJwzgfID6Q@cluster0.ze6uv.mongodb.net/').then(()=>console.log("DB Connected"));
    
 }
 
